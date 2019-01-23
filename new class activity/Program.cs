80faf5b2425d499f302dd50a4f03e69f68f58bfe
Sawan <sawanturan645@gmail.com>
2019-01-23 11:45:29 -0500

    first time change

--- new class activity/Program.cs
+++ new class activity/Program.cs
@@ -10,6 +10,16 @@ namespace new_class_activity
     {
         static void Main(string[] args)
         {
+            int i = 0;
+            int sum = 0;
+
+            while (i <= 5)
+            {
+                sum += i;
+                i++;
+            }
+            Console.WriteLine("Sum = {0}", sum);
+            Console.ReadLine();
         }
     }
 }
